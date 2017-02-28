20fb0f4c9b38e97ea8c587a7dbe51034b89a2e3e
Brian Mont <briansmont830@gmail.com>
2017-02-28 16:21:05 +0000

    fixed extra row which messed up grid

--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -1,6 +1,4 @@
 var React = require('react');
-var InfiniteGrid = require('react-infinite-grid');
-
 var Faker = require('faker');
 
 var catalog = [
@@ -18,10 +16,6 @@ var catalog = [
   {name: Faker.commerce.productName(), price: Faker.commerce.price(), image: Faker.image.city()},
 ];
 
-var items = [];
-for (var i = 0; i < 11; i++) {
-  items.push('This is #' + i);
-}
 var Products = React.createClass({
   renderCatalog: function() {
     var products = [];
@@ -32,11 +26,10 @@ var Products = React.createClass({
         // image: catalog[i].image,
       };
       products.push(
-        <div className="row comp-pad">
-          <div className="column">
-            <p><strong>{product.name}:</strong> Price: ${product.price}</p> <button className="button">Buy Now!</button>
-          </div>
-        </div>  
+        <div className="column">
+          <p><strong>{product.name}:</strong> Price: ${product.price}</p> <button className="button">Buy Now!</button>
+        </div>
+      
         // <img src={product.image} height="20%" width="20%" alt="product" className="comp-pad"></img>
       );
     }
@@ -49,12 +42,10 @@ var Products = React.createClass({
         <h1 className="center-text"><u>Products</u></h1>
         <div className="row small-up-1 medium-up-2 large-up-3">
           {this.renderCatalog()}
-          <InfiniteGrid itemClassName={"item"} entries={items}/>
         </div>
       </div>
     );
   }
 });
-
 
 module.exports = Products;
