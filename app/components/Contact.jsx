451019f55be7a5f2fec62f61c512b1460c8c9c0a
Brian Mont <briansmont830@gmail.com>
2017-02-28 21:39:35 +0000

    simple form attempt #1

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,12 +1,19 @@
 var React = require('react');
 
 var Contact = React.createClass({
+  
+  handleSubmit: function(e) {
+    e.preventDefault();
+    console.log('submitted!');
+  },
+  
   render: function() {
     return (
       <div className="comp-pad">
         <div className="small-centered small-11 medium-6 large-5">
-          <h1>How can we help?</h1>
-          <form onSubmit={this.handleSubmit}>
+          <h1>What's up?</h1>
+          <form action="https://getsimpleform.com/messages?form_api_token=bf9ac70ecd711befee6b55ce8665958c" method="post">
+            <input type='hidden' name='redirect_to' value='/' />
             <input autoFocus type="text" ref="fullName" placeholder="Enter Full Name"/>
             <input type="text" ref="email" placeholder="Email Address" />
             <input type="number" ref="phoneNumber" placeholder="Phone Number" />
