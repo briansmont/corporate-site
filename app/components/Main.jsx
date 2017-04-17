479bcebda7bcfc49291c4fce345e5a66c0dcc09c
Brian Mont <briansmont830@gmail.com>
2017-04-17 14:58:04 +0000

    added google font Asar for all child components that will change in the SPA

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -6,7 +6,7 @@ var Main = (props) => {
     <div>
       <Nav/>
       <div className="row">
-        <div>
+        <div className="content">
           {props.children}
         </div>
       </div>
