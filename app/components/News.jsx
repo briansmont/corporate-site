6db4246ed0d80ef44c5bf18687ccc67dc2949b37
Brian Mont <briansmont830@gmail.com>
2017-04-25 16:28:26 +0000

    links working, need to reformat so each source has a clickable title, and then the expand/hide button

--- app/components/News.jsx
+++ app/components/News.jsx
@@ -87,32 +87,39 @@ var News = React.createClass({
     });
   },
   
-  showBBC: function() {
+  showBBC: function(e) {
+    e.preventDefault();
     document.getElementById("bbc-heading").innerHTML = bbcObject.title;
     document.getElementById("bbc-description").innerHTML = bbcObject.desc;
   },
-  showESPN: function() {
+  showESPN: function(e) {
+    e.preventDefault();
     document.getElementById("espn-heading").innerHTML = espnObject.title;
     document.getElementById("espn-description").innerHTML = espnObject.desc;
   },
-  showCNBC: function() {
+  showCNBC: function(e) {
+    e.preventDefault();
     document.getElementById("cnbc-heading").innerHTML = cnbcObject.title;
     document.getElementById("cnbc-description").innerHTML = cnbcObject.desc;
-    document.getElementById("cnbc-more").innerHTML = "See more at" + <a href="www.cnbc.com" target="_blank">CNBC</a>;
+    document.getElementById("cnbc-more").innerHTML = "See more at" + <a href="https://www.cnbc.com" target="_blank">CNBC</a>;
   },
-  showHacker: function() {
+  showHacker: function(e) {
+    e.preventDefault();
     document.getElementById("hacker-heading").innerHTML = hackerObject.title;
     document.getElementById("hacker-description").innerHTML = hackerObject.desc;
   },
-  showAP: function() {
+  showAP: function(e) {
+    e.preventDefault();
     document.getElementById("ap-heading").innerHTML = apObject.title;
     document.getElementById("ap-description").innerHTML = apObject.desc;
   },
-  showBreit: function() {
+  showBreit: function(e) {
+    e.preventDefault();
     document.getElementById("breit-heading").innerHTML = breitObject.title;
     document.getElementById("breit-description").innerHTML = breitObject.desc;
   },
-  showAljaz: function() {
+  showAljaz: function(e) {
+    e.preventDefault();
     document.getElementById("aljaz-heading").innerHTML = alJazObject.title;
     document.getElementById("aljaz-description").innerHTML = alJazObject.desc;
   },
@@ -135,7 +142,7 @@ var News = React.createClass({
           <div className="row">
             <div id="bbc-heading" className="heading"></div>
             <div id="bbc-description" className="desc"></div>
-            <p>See more at <a href="www.bbc.com" target="_blank">BBC</a></p>
+            <p>See more at <a href="https://www.bbc.com" target="_blank">BBC</a></p>
           </div>
         </div>
         
@@ -146,7 +153,7 @@ var News = React.createClass({
           <div className="row">
             <div id="espn-heading" className="heading"></div>
             <div id="espn-description" className="desc"></div>
-            <p>See more at <a href="www.espn.com" target="_blank">ESPN</a></p>
+            <p>See more at <a href="https://www.espn.com" target="_blank">ESPN</a></p>
           </div>
         </div>
         
@@ -157,7 +164,7 @@ var News = React.createClass({
           <div className="row">
             <div id="cnbc-heading" className="heading"></div>
             <div id="cnbc-description" className="desc"></div>
-            <p id="cnbc-more">See more at <a href="www.cnbc.com" target="_blank">CNBC</a></p>
+            <p id="cnbc-more"></p>
           </div>
         </div>
         
