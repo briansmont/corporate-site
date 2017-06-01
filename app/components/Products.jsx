3eb8f15d1a2a55f01c727e431bc6bfa13211bc86
Brian Mont <briansmont830@gmail.com>
2017-06-01 18:27:31 +0000

    added productPage link to contact so users can request new products

--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 import * as Redux from 'react-redux';
+var {Link} = require('react-router');
 
 import SearchProducts from 'SearchProducts';
 import ProductList from 'ProductList';
@@ -26,6 +27,7 @@ export var Products = React.createClass({
     return (
       <div className="comp-pad">
         <h1 className="page-title">Products</h1>
+        <p className="center-text"><em>If you can't find what you're looking for, <Link to="/contact">request it here!</Link></em></p>
         <SearchProducts/>
         <ProductList/>
         <div className="column small-centered small-11 medium-6 large-5">
